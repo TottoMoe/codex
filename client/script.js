486edd5cc28d9e9e532ab490e63f5b735535c524
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2023-02-24 21:02:28 -0800

    change update

--- client/script.js
+++ client/script.js
@@ -1,21 +1,23 @@
-import bot from './assets/bot.svg';
-import user from './assets/user.svg';
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
 
-const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_container');
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
 
 let loadInterval;
 
 function loader(element) {
-  element.textContent = '';
+  element.textContent = "";
 
   loadInterval = setInterval(() => {
-    element.textContent += '.';
+    // Update the text content of the loading indicator
+    element.textContent += ".";
 
-    if (element.textContent === '....') {
-      element.textContent = '';
+    // If the loading indicator has reached three dots, reset it
+    if (element.textContent === "....") {
+      element.textContent = "";
     }
-  }, 300)
+  }, 300);
 }
 
 function typeText(element, text) {
@@ -24,13 +26,16 @@ function typeText(element, text) {
   let interval = setInterval(() => {
     if (index < text.length) {
       element.innerHTML += text.charAt(index);
-      index++
+      index++;
     } else {
       clearInterval(interval);
     }
-  }, 20)
+  }, 20);
 }
 
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
 function generateUniqueId() {
   const timestamp = Date.now();
   const randomNumber = Math.random();
@@ -40,21 +45,19 @@ function generateUniqueId() {
 }
 
 function chatStripe(isAi, value, uniqueId) {
-  return (
-    `
-      <div class="wrapper ${isAi && 'ai'}">
-        <div class="chat">
-          <div class="profile">
-            <img 
-              src=${isAi ? bot : user}
-              alt="${isAi ? 'bot' : 'user'}"
-            />
-          </div>
-          <div class="message" id=${uniqueId}>${value}</div>
+  return `
+        <div class="wrapper ${isAi && "ai"}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? "bot" : "user"}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
         </div>
-      </div>
-    `
-  )
+    `;
 }
 
 const handleSubmit = async (e) => {
@@ -63,21 +66,23 @@ const handleSubmit = async (e) => {
   const data = new FormData(form);
 
   // user's chatstripe
-  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
 
+  // to clear the textarea input
   form.reset();
 
   // bot's chatstripe
   const uniqueId = generateUniqueId();
   chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
 
+  // to focus scroll to the bottom
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
+  // specific message div
   const messageDiv = document.getElementById(uniqueId);
 
+  // messageDiv.innerHTML = "..."
   loader(messageDiv);
-
-  // fetch data from server -> bot's response
 
   const response = await fetch("https://codex-zmp0.onrender.com/", {
     method: "POST",
@@ -90,25 +95,24 @@ const handleSubmit = async (e) => {
   });
 
   clearInterval(loadInterval);
-  messageDiv.innerHTML = '';
+  messageDiv.innerHTML = " ";
 
-  if(response.ok) {
+  if (response.ok) {
     const data = await response.json();
-    const parsedData = data.bot.trim();
+    const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
 
     typeText(messageDiv, parsedData);
   } else {
     const err = await response.text();
 
     messageDiv.innerHTML = "Something went wrong";
-
     alert(err);
   }
-}
+};
 
-form.addEventListener('submit', handleSubmit);
-form.addEventListener('keyup', (e) => {
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
   if (e.keyCode === 13) {
     handleSubmit(e);
   }
-})
+});
